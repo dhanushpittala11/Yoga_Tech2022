24ad55c223311728ce3a1782d293dc7e68c0a56e
dhanushpittala11 <108401638+dhanushpittala11@users.noreply.github.com>
2022-06-29 17:49:31 +0530

    Create client.py

---
+++ Clients/Python_clients/client.py
@@ -0,0 +1,25 @@
+import websocket
+import json
+import time
+import numpy as np
+
+
+count =0
+data = []
+def on_message(wsapp, message):
+    print(message)
+    count +=1
+    if count == 5:
+        with open('output.json',"w") as f:
+            json.dumps(data,f)
+    data.append(message)
+
+wsapp = websocket.WebSocketApp("ws://192.168.0.116:7892/", on_message=on_message)
+wsapp.run_forever()
+
+if count == 5:
+    with open('output.json',"w") as f:
+        json.dumps(data,f)
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
